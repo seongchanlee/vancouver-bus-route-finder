da257cb82a6c53d482ba1988672eb0192fbbe6b3
Seongchan Lee <seongchan.lee96@icloud.com>
2018-10-16 02:30:26 -0700

    Rebase on Hanae's branch

--- Main.hs
+++ Main.hs
@@ -1,23 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
--- import Network.HTTP.Conduit
--- import qualified Data.ByteString.Lazy as L
 import System.IO
-
-apiUrl = "http://api.translink.ca/rttiapi/v1/"
-apiKey = "apikey="
-
--- getStopsNearBusLoop = do
---     let query = apiUrl ++ "stops?" ++ apiKey ++ "&lat=49.268342&long=-123.250015"
---     simpleHttp query >>= L.putStr
-
--- main = getStopsNearBusLoop
 
 main = do
  getOriginFromUser
  getDestFromUser
 
-getOriginFromUser :: IO String 
+getOriginFromUser :: IO String
 getOriginFromUser = do
  putStrLn "Where are you leaving from?"
  origin <- getLineFixed
