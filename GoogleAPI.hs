f3cf97580a4315b4b2dbf5bee9f092aa08206e90
Seongchan Lee <seongchan.lee96@icloud.com>
2018-10-17 03:18:51 -0700

    Connected main I/O with API calls

--- GoogleAPI.hs
+++ GoogleAPI.hs
@@ -8,19 +8,24 @@ import Network.HTTP.Conduit
 import Network.HTTP.Base
 import Network.HTTP.Headers
 import Data.Maybe
-import qualified Data.ByteString.Lazy as B
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Char8 as BS
+import qualified JsonParser as JP
 
 directionsUrl = "https://maps.googleapis.com/maps/api/directions/json?"
 
 {- DO NOT PUSH API KEY -}
 apiKey = ""
 
-getDirection origin destination = do
+getDirectionFromApi :: String -> String -> IO(LBS.ByteString)
+getDirectionFromApi origin destination = do
   {- From busloop to Granville station as a sample for now -}
   initReq <- parseUrl (directionsUrl)
   let r = initReq {method = "GET"}
-  let request = setQueryString [("origin", Just origin)
-                                ,("destination", Just destination)
+  let request = setQueryString [("origin", Just (BS.pack origin))
+                                ,("destination", Just (BS.pack destination))
                                 ,("mode", Just "transit")
                                 ,("key", Just apiKey)] r
 
@@ -28,6 +33,19 @@ getDirection origin destination = do
   res <- httpLbs request manager
   return . responseBody $ res
 
-getDirectionInExternalJson origin destination = do
-  routeInJson <- getDirection origin destination
-  B.writeFile "route.json" routeInJson
+storeDirectionInExternalJson :: String -> String -> IO()
+storeDirectionInExternalJson origin destination = do
+  routeInJson <- getDirectionFromApi origin destination
+  LBS.writeFile "route.json" routeInJson
+
+routes :: Value -> Parser [JP.Route]
+routes = withObject "Routes" $ \o -> o .: "routes"
+
+getDirectionFromFile :: String -> String -> IO ([JP.Route])
+getDirectionFromFile origin destination = do
+  storeDirectionInExternalJson origin destination
+  bs <- LBS.readFile "route.json"
+  let maybeRoutes = parseMaybe routes =<< decode bs
+  let routeList = JP.checkRoutes maybeRoutes
+  return routeList
+
--- Main.hs
+++ Main.hs
@@ -3,20 +3,19 @@
 module Main where
 
 import System.IO
-
-main = do
- getOriginFromUser
- getDestFromUser
+import qualified GoogleAPI as GAPI
+import qualified JsonParser as JP
+import qualified Data.Text as T
 
 getOriginFromUser :: IO String
 getOriginFromUser = do
- putStrLn "Where are you leaving from?"
+ putStrLn "Where are you leaving from?:"
  origin <- getLineFixed
  return origin
 
 getDestFromUser :: IO String
 getDestFromUser = do
- putStrLn "Where would you like to go?"
+ putStrLn "Where would you like to go?:"
  dest <- getLineFixed
  return dest
 
@@ -31,3 +30,71 @@ fixdel st
 remdel ('\DEL':r) = r
 remdel (a:'\DEL':r) = r
 remdel (a:r) = a: remdel r
+
+askRestart :: IO String
+askRestart = do
+ putStrLn "Would you like to do another search? (y/n):"
+ response <- getLineFixed
+ return response
+
+unshellRoute :: [JP.Route] -> JP.Route
+unshellRoute (h:t) = h
+
+unshellLeg :: [JP.Leg] -> JP.Leg
+unshellLeg (h:t) = h
+
+printArrivalTime :: JP.Leg -> IO ()
+printArrivalTime leg = putStrLn("Arrival time: " ++ (T.unpack (JP.arrivalTime (leg))))
+
+printDepartureTime :: JP.Leg -> IO ()
+printDepartureTime leg = putStrLn("Departure time: " ++ (T.unpack (JP.departureTime (leg))))
+
+printDuration :: JP.Leg -> IO ()
+printDuration leg = putStrLn("Duration: " ++ (T.unpack (JP.duration (leg))))
+
+printSteps :: [JP.Step] -> IO ()
+printSteps steps = putStrLn("Instruction: " ++ "\n" ++ (stepsToString 1 steps))
+
+stepsToString :: Int -> [JP.Step] -> [Char]
+stepsToString _ [] = ""
+stepsToString n (h:t) = (((show n) ++ ". "++ (T.unpack(JP.instruction (h)))) ++ "\n") ++ (stepsToString (n+1) t)
+
+printNewLine :: IO ()
+printNewLine = putStrLn("")
+
+initMenu :: IO()
+initMenu = do
+  origin <- getOriginFromUser
+  dest <- getDestFromUser
+  if (origin /= "" && dest /= "") then do
+    route <- GAPI.getDirectionFromFile origin dest
+    let leg = unshellLeg ((JP.legs (unshellRoute route)))
+
+    printNewLine
+    printDuration leg
+    printDepartureTime leg
+    printArrivalTime leg
+    printNewLine
+    printSteps (JP.steps leg)
+
+    response <- askRestart
+
+    if response == "y" then (do
+      printNewLine
+      initMenu)
+    else if response == "n" then (do
+      printNewLine
+      putStrLn("Exiting.")
+      return ())
+    else (do
+      printNewLine
+      putStrLn("Invalid input. Exiting."))
+
+  else do
+    putStrLn("Invalid input. Please try again.")
+    printNewLine
+    initMenu
+
+main :: IO()
+main = do
+ initMenu
